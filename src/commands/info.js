1663b893bb288411607b7ed651249cf3dff01f01
NFG004 <opticstep@gmail.com>
2021-12-16 02:13:32 -0500

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,10 +1,6 @@
 const talkedRecently = new Set();
+const { MessageEmbed } = require('discord.js');
 module.exports = async (msg) => {
-	 if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
-
-           // the user can type the command ... your command code goes here :)
 			let helpMsg = '**FUCK YOUUU** \n \n';
 			await msg.channel.send(helpMsg);
         // Adds the user to the set so that they can't talk for a minute
@@ -13,6 +9,26 @@ module.exports = async (msg) => {
           // Removes the user from the set after a minute
           talkedRecently.delete(msg.author.id);
         }, 60000);
+		
+	const exampleEmbed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+channel.send({ embeds: [exampleEmbed] });
     }
 	
 
