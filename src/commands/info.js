ad0ed5d13add42107890d6c6fc2fc3ff306ab015
NFG004 <opticstep@gmail.com>
2021-12-15 23:53:48 -0500

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,5 +1,25 @@
+const { MessageEmbed } = require('discord.js');
 module.exports = async (msg) => {
 	let helpMsg = '**this shit is broken, needs firebase db stop crying** \n \n';
 	
 	await msg.channel.send(helpMsg);
 };
+const exampleEmbed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+channel.send({ embeds: [exampleEmbed] });
