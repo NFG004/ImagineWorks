113dfc48c4efd58c3d8c50be1a1dda0053442900
NFG004 <opticstep@gmail.com>
2021-12-19 12:44:41 -0500

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,7 +1,9 @@
 const talkedRecently = new Set();
 module.exports = async (msg) => {
+	if !(talkedRecently.has(msg.author.id) {
 			let helpMsg = '**FUCK YOUUU** \n \n';
 			await msg.channel.send(helpMsg);
+	}
         // Adds the user to the set so that they can't talk for a minute
         talkedRecently.add(msg.author.id);
         setTimeout(() => {
