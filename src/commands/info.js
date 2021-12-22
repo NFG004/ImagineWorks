4f3b93aca91d66767316c3693f41de50a38d7931
NFG004 <opticstep@gmail.com>
2021-12-22 14:19:00 -0500

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,11 +1,31 @@
 const talkedRecently = new Set();
+const Discord = require('discord.js');
 module.exports = async (msg) => {
 	if (talkedRecently.has(msg.author.id)) {
 			
 	}
 	else {
-			let helpMsg = '**FUCK YOUUU** \n \n';
-			await msg.channel.send(helpMsg);
+			//let helpMsg = '**FUCK YOUUU** \n \n';
+			//await msg.channel.send(helpMsg);
+			const exampleEmbed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Some title')
+			.setURL('https://discord.js.org/')
+			.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+			.setDescription('Some description here')
+			.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+			.addFields(
+				{ name: 'Regular field title', value: 'Some value here' },
+				{ name: '\u200B', value: '\u200B' },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+			)
+			.addField('Inline field title', 'Some value here', true)
+			.setImage('https://i.imgur.com/wSTFkRM.png')
+			.setTimestamp()
+			.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+			awai msg.channel.send(exampleEmbed);	
 	}
         // Adds the user to the set so that they can't talk for a minute
         talkedRecently.add(msg.author.id);
