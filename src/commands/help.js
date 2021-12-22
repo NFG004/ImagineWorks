086e32779313f6a9bac77c54f07df9cfa65b43b1
NFG004 <opticstep@gmail.com>
2021-12-22 14:09:03 -0500

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,17 +4,17 @@ module.exports = async (msg) => {
 			
 	}
 	else {	
-	let helpMsg = '**More commands soon** \n \n';
-	helpMsg += '` !imagine @Killer @Victim ` - Log a team kill. You can also include a reason \n \n';
-	helpMsg += '` !tkdel ` - Display last 5 logged kills and allow you to remove any of them via button click. Tag a user for specific TK removal \n \n';
-	helpMsg += '` !tkreset ` - Resets all TK logs \n \n';
-	helpMsg += '` !tk ` - List of all team kill stats\n \n';
-	helpMsg += '` !tk @User ` - List of all team kills for a specific user \n \n';
-	helpMsg += '` !tkk ` - Scoreboard of the users with the most team kills \n \n';
-	helpMsg += '` !tkd ` - Scoreboard of the users with the most team deaths \n \n';
-	await msg.channel.send(helpMsg);
-	 // Adds the user to the set so that they can't talk for a minute
+		let helpMsg = '**More commands soon** \n \n';
+		helpMsg += '` !imagine @Killer @Victim ` - Log a team kill. You can also include a reason \n \n';
+		helpMsg += '` !tkdel ` - Display last 5 logged kills and allow you to remove any of them via button click. Tag a user for specific TK removal \n \n';
+		helpMsg += '` !tkreset ` - Resets all TK logs \n \n';
+		helpMsg += '` !tk ` - List of all team kill stats\n \n';
+		helpMsg += '` !tk @User ` - List of all team kills for a specific user \n \n';
+		helpMsg += '` !tkk ` - Scoreboard of the users with the most team kills \n \n';
+		helpMsg += '` !tkd ` - Scoreboard of the users with the most team deaths \n \n';
+		await msg.channel.send(helpMsg);
 	}
+	// Adds the user to the set so that they can't talk for a minute
         talkedRecently.add(msg.author.id);
         setTimeout(() => {
           // Removes the user from the set after a minute
