9bb926ec2bdbce0232b7e08906a7940847fe17b1
NFG004 <opticstep@gmail.com>
2021-12-22 07:24:51 -0500

    Update reset.js

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -1,6 +1,10 @@
 const firebase = require('firebase');
 const talkedRecently = new Set();
 module.exports = async (msg) => {
+	if (talkedRecently.has(msg.author.id)) {
+			
+	}
+	else {	
 	firebase.firestore().collection('kills').where('serverId', '==', msg.guild.id).get()
 		.then((killsQuery) => {
 			killsQuery.forEach((doc) => {
@@ -15,6 +19,7 @@ module.exports = async (msg) => {
 			console.log('Error getting documents: ', error);
 		});
 		// Adds the user to the set so that they can't talk for a minute
+	}
         talkedRecently.add(msg.author.id);
         setTimeout(() => {
           // Removes the user from the set after a minute
