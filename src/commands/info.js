eb46c206c60e5069bb87608859d676317610e2d4
NFG004 <opticstep@gmail.com>
2021-12-23 20:56:28 -0500

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -9,21 +9,21 @@ module.exports = async (msg) => {
 			//await msg.channel.send(helpMsg);
 			const exampleEmbed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
-			.setTitle('Some title')
-			.setURL('https://discord.js.org/')
-			.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-			.setDescription('Some description here')
-			.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-			.addFields(
-				{ name: 'Regular field title', value: 'Some value here' },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-			)
-			.addField('Inline field title', 'Some value here', true)
-			.setImage('https://i.imgur.com/wSTFkRM.png')
+			//.setTitle('Top Teamkillers')
+			//.setURL('https://discord.js.org/')
+			.setAuthor('Top Teamkillers')
+			.setDescription('1. Retard \n 2. Retard \n 3. Retard 3')
+			//.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+			//.addFields(
+			//	{ name: 'Regular field title', value: 'Some value here' },
+			//	{ name: '\u200B', value: '\u200B' },
+			//	{ name: 'Inline field title', value: 'Some value here', inline: true },
+			//	{ name: 'Inline field title', value: 'Some value here', inline: true },
+			//)
+			//.addField('Inline field title', 'Some value here', true)
+			//.setImage('https://i.imgur.com/wSTFkRM.png')
 			.setTimestamp()
-			.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+			.setFooter('Couldn\'t be me', 'https://i.imgur.com/wSTFkRM.png');
 
 			await msg.channel.send(exampleEmbed);	
 	}
